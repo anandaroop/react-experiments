afacca5aa02b503d10faf37ee155eb8ebe3cc63b
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2017-05-04 06:59:24 -0400

    mock layout ready for testing

--- src/App.js
+++ src/App.js
@@ -1,16 +1,63 @@
 import React, { Component } from 'react';
 import styled from 'styled-components'
+import artworks from './artworks.json'
 
+const Artwork = ({className, title}) => (
+  <div className={className}>
+    <h1>{title}</h1>
+  </div>
+)
+
+const StyledArtwork = styled(Artwork)`
+  flex: 0 1 200px;
+  height: 200px;
+  margin: 1em;
+  border: solid 1px gray;
+  h1 {
+    font-size: 1.2em;
+  }
+`
+
+const Grid = styled.div`
+  display: flex;
+  flex-flow: row wrap;
+`
+
+const Button = styled.a`
+  display: inline-block;
+  padding: 1em;
+  border: solid 1px gray;
+  margin: 1em;
+`
+
+const Modal = () => <h1>MODAL!</h1>
 
 class App extends Component {
+  constructor () {
+    super()
+    this.state = {
+      isModal: false
+    }
+    this.toggle = this.toggle.bind(this)
+  }
+
+  toggle () {
+    this.setState({
+      isModal: !this.state.isModal
+    })
+  } 
+
   render() {
     return (
       <div>
-        App!
+        <Button href="#" onClick={this.toggle}>modal</Button>      
+        { this.state.isModal && <Modal />}
+        <Grid>
+          {artworks.map(w => <StyledArtwork key={w.id} {...w} />)}
+        </Grid>
       </div>
     );
   }
 }
 
 export default App;
-
