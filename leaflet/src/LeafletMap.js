453ef84cc91bf2ffd8e9f38d54bc1d6c16f7faae
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2017-07-01 01:38:44 -0400

    Fetch real bbox shows from the api

--- leaflet/src/LeafletMap.js
+++ leaflet/src/LeafletMap.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import _ from 'lodash'
 import L from 'leaflet'
 import 'leaflet-providers'
 
@@ -9,26 +10,61 @@ class LeafletMap extends React.Component {
     this.state = {
       lat,
       lng,
-      zoom
+      zoom,
+      shows: []
     }
     this.updateStateFromMap = this.updateStateFromMap.bind(this)
+    this.fetchShows = this.fetchShows.bind(this)
+  }
+
+  fetchShows() {
+    const bounds = this._map.getBounds()
+    const corners = [
+      bounds.getNorthWest().wrap(),
+      bounds.getNorthEast().wrap(),
+      bounds.getSouthEast().wrap(),
+      bounds.getSouthWest().wrap(),
+      bounds.getNorthWest().wrap()
+    ]
+    const boundingBoxString = _.flatten(corners.map(p => [p.lat, p.lng])).join(',')
+    const uri = `https://api.artsy.net/api/v1/shows?status=current&geo_within=${boundingBoxString}&size=50`
+    window
+      .fetch(uri, {
+        credentials: 'include'
+      })
+      .then(resp => resp.json())
+      .then(data => {
+        this.setState({ shows: data })
+        console.log('fetched', data.length, 'shows from', uri)
+      })
   }
 
   componentDidMount() {
     // instantiate
     const { lat, lng, zoom } = this.state
-    this.map = L.map(this.props.id).setView([lat, lng], zoom)
-    L.tileLayer.provider('CartoDB.Positron').addTo(this.map)
+    this._map = L.map(this.props.id).setView([lat, lng], zoom)
+    this._markers = L.layerGroup().addTo(this._map)
+    L.tileLayer.provider('CartoDB.Positron').addTo(this._map)
 
     // events
-    this.map.on('moveend', this.updateStateFromMap)
+    this._map.on('moveend', this.updateStateFromMap)
+    this._map.on('moveend', _.debounce(this.fetchShows, 500))
+  }
+
+  componentDidUpdate(_prevProps, prevState) {
+    if (!_.isEqual(this.state.shows, prevState.shows)) {
+      this._markers.clearLayers()
+      this.state.shows.forEach(s => {
+        L.circleMarker(s.coordinates).addTo(this._markers)
+      })
+    }
   }
 
   updateStateFromMap() {
     this.setState({
-      lat: this.map.getCenter().lat,
-      lng: this.map.getCenter().lng,
-      zoom: this.map.getZoom()
+      lat: this._map.getCenter().lat,
+      lng: this._map.getCenter().lng,
+      zoom: this._map.getZoom()
     })
   }
 
