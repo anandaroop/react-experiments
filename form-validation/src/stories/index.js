eb329b9492544ef4577745f6be922dc8edc78aa6
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2017-07-11 17:00:46 -0400

    Add desc to story

--- form-validation/src/stories/index.js
+++ form-validation/src/stories/index.js
@@ -2,11 +2,14 @@ import React from 'react'
 import { storiesOf, action, linkTo } from '@kadira/storybook'
 import UncontrolledForm from '../components/UncontrolledForm'
 
-storiesOf('UncontrolledForm', module).add('default', () =>
-  <UncontrolledForm
-    onSubmit={e => {
-      alert('submitted!')
-      return false
-    }}
-  />
+storiesOf('UncontrolledForm', module).add(null, () =>
+  <div>
+    <p>An uncontrolled, unstyled, dumb little form component</p>
+    <UncontrolledForm
+      onSubmit={e => {
+        alert('submitted!')
+        return false
+      }}
+    />
+  </div>
 )
