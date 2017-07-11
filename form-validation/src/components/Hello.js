49ddd3145bb8cfcc83e6d45ff002c4257f558bfc
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2017-07-11 17:00:26 -0400

    Remove hello component

--- form-validation/src/components/Hello.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import styled from 'styled-components'
-
-const Hello = () => <Heading>Hello</Heading>
-
-const Heading = styled.h1`color: lightGray;`
-
-export default Hello
--- form-validation/src/stories/index.js
+++ form-validation/src/stories/index.js
@@ -1,9 +1,6 @@
 import React from 'react'
 import { storiesOf, action, linkTo } from '@kadira/storybook'
-import Hello from '../components/Hello'
 import UncontrolledForm from '../components/UncontrolledForm'
-
-storiesOf('Hello', module).add('default', () => <Hello />)
 
 storiesOf('UncontrolledForm', module).add('default', () =>
   <UncontrolledForm
