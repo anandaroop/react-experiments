d728791e0eb128310270938b7250025eed0a0359
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2017-09-26 19:16:55 -0400

    more WIP

--- genome-diff/src/App.js
+++ genome-diff/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import styled from 'styled-components'
 import './App.css'
 import data from './luc-paradis.json'
+import _ from 'lodash'
 
 class App extends Component {
   constructor(props) {
@@ -19,7 +20,6 @@ class App extends Component {
     const currentChange = this.state.changes[currentIndex]
     return (
       <div className="App">
-        <p>Genome updates</p>
         <p>{description}</p>
         <Change {...currentChange} />
       </div>
@@ -28,24 +28,57 @@ class App extends Component {
 }
 
 const Change = ({ timestamp, before, after }) => {
+  const allGeneNames = Object.keys(Object.assign({}, before.genes, after.genes)).sort()
   return (
     <div>
       <Timestamp>{timestamp}</Timestamp>
       <Diff>
-        <Before>{JSON.stringify(before.genes)}</Before>
-        <After>{JSON.stringify(after.genes)}</After>
+        <Genome allGeneNames={allGeneNames} genes={before.genes} />
+        <Genome allGeneNames={allGeneNames} genes={after.genes} />
       </Diff>
     </div>
   )
 }
 
-const Timestamp = styled.div``
+const Genome = ({ className, allGeneNames, genes }) => {
+  console.log(allGeneNames, _.keys(genes))
+  return (
+    <div className={className}>
+      {allGeneNames.map(name => {
+        return _.has(genes, name) ? (
+          <Gene key={Math.random()} name={name} value={genes[name]} />
+        ) : (
+          <Blank />
+        )
+      })}
+    </div>
+  )
+}
+
+const Gene = ({ name, value }) => (
+  <div>
+    {name}
+    {value}
+  </div>
+)
+
+const Blank = styled.div`
+min-height: 1em;
+background: #eee;
+`
+
+const Timestamp = styled.div`
+  font-size: smaller;
+  padding: 0.5em;
+  background: #ccc;
+  color: white;
+  text-align: center;
+  margin: 0 0 1em 0;
+`
 
 const Diff = styled.div`display: flex;`
 
-const Genome = styled.div``
-
-const Before = Genome.extend`color: #900;`
-const After = Genome.extend`color: #090;`
+const Before = styled(Genome)`color: #900;`
+const After = styled(Genome)`color: #090;`
 
 export default App
